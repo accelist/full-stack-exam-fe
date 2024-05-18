dec3fc25dc60ef9acbd562413aeda3dec467656e
Robieeee <ferdinandrobertus@gmail.com>
2024-05-18 22:54:35 +0700

    Styling Update

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -12,7 +12,7 @@ import "swiper/css/scrollbar";
 const newsComponent: Page = () => {
   return (
     <div>
-      <Title>Login</Title>
+      <Title>News - CGV Cinema</Title>
       <div className="flex-1 flex relative">
         <div
           className=" flex justify-around gap-16 w-full h-10"
